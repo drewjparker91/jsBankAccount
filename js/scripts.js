fdb211c81cca268216098e45c66147a688b157f4
drewparker <drewjparker91@gmail.com>
2020-08-25 14:31:15 -0700

    Add forms to HTML and being scripts
    Co-authored-by: Mike Manchee <mikemanchee@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+// Business Logic BankAccount
+function BankAccount() {
+  this.accounts = [],
+  this.accoutId = 0
+}
+
+
+
+
+// Business Logic Account
+// userName, accountBalance, accountNumber
+
+function Accounts(userName) {
+  this.userName = userName;
+  this.account = { balance: , }
+}
+
+
+
+// User Interface
+$(documnet).ready(function() {
+
+});
+
+BankAccount.prototype.deposit = function(deposit) {
+  this.account += deposit;
+}
+
+BankAccount.prototype.withdrawal = function(withdrawal)
+  this.balance -= withdrawal;
+
+Function balanceCheck(user, deposit, withdrawal){
+  if (d)
+}
