c477456e9471033228c1dd5c1fec9cc4941692b3
drewparker <drewjparker91@gmail.com>
2020-08-25 16:39:39 -0700

    Add prototypes and functions co-authored-by: Mike Manchee <mikemanchee@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,79 @@
 // Business Logic BankAccount
 function BankAccount() {
   this.accounts = [],
-  this.accoutId = 0
+  this.accountId = 0
+}
+
+BankAccount.prototype.addAccount = function(account) {
+  account.id = this.assignId();
+  this.accounts.push(account);
+}
+
+BankAccount.prototype.assignId = function() {
+  this.accountId += 1;
+  return this.accountId;
+}
+
+BankAccount.prototype.findAccount = function(id) {
+  for (let i=0; i< this.accounts.length; i++) {
+    if (this.accounts[i]) {
+      if (this.accounts[i].id == id) {
+        return this.accounts[i];
+      }
+    }
+  };
+  return false;
+}
+
+Accounts.prototype.moreDeposit = function(deposit) {
+  this.account += deposit; 
+}
+
+Accounts.prototype.withdrawal = function(withdrawal) {
+  this.account -= withdrawal;
+}
+
+function balanceCheck(userID, deposit, withdrawal){
+  const accounts = bankAccount.findAccount(userID);
+  if (deposit === 0 && withdrawal ===0) {                           // error if 0 entered
+    $("#error").text("You must add a value to deposit or withdrawal from your acount");
+  } else if (deposit > 0 && withdrawal > 0) {                       // error if something in both
+    $("#error").text("Please add a value into either 'Deposit Amount' or 'Withdrawal Amount'.");
+  } else if (deposit > 0) {                                         // something in deposit only
+    accounts.moreDeposit(deposit);
+  } else if (withdrawal > 0) {                                      // something in withdrawl only
+    accounts.withdrawal(withdrawal);
+  }
 }
 
 
-
-
 // Business Logic Account
-// userName, accountBalance, accountNumber
+// userName, accountBalance
 
-function Accounts(userName) {
+function Accounts(userName, balance) {
   this.userName = userName;
-  this.account = { balance: , }
+  this.account = balance
 }
 
 
 
 // User Interface
 $(documnet).ready(function() {
+  $("form#new-account").SVGNumberList(function() {
+    let name = $("input#name").val();
+    let initialDeposit = $("input#initialDeposit").val();
 
+    let newAccout = new Accounts(name, initialDeposit);
+    
+  });
+  $("form#addTake").submit(function() {
+    let deposit = $("input#deposit").val();
+    let withdrawl = $("input#withdrawl").val();
+
+    balanceCheck
+  })
 });
 
-BankAccount.prototype.deposit = function(deposit) {
-  this.account += deposit;
-}
 
-BankAccount.prototype.withdrawal = function(withdrawal)
-  this.balance -= withdrawal;
 
-Function balanceCheck(user, deposit, withdrawal){
-  if (d)
-}
+
